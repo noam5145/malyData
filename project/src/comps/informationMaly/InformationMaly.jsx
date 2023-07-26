349321c6e3a97c10a222a60c0643a9cb6b4291f1
noam5145 <109206563+noam5145@users.noreply.github.com>
2023-07-26 20:35:20 +0300

    start

---
+++ project/src/comps/informationMaly/InformationMaly.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "./informשtion.css";
+import { Link } from "react-router-dom";
+
+
+export default function InformationMaly() {
+  return (
+    <div className="row content">
+      <div className="col-4">
+        <div className="d-flex justify-content-center align-items-center bg-secondary m-2">
+          <h4 className="text-info"> ייעוד מל"י</h4>
+        </div>
+        <div className="h-100 m-2  bg-secondary">
+          <h5 className="d-flex justify-content-center align-items-center h-25">
+            {" "}
+            הכנת כוחות היבשה באופן חיילי ורב זרועי למלחמה
+          </h5>
+          <span className="contantImg  d-flex justify-content-center">
+            <img src="https://vanl.ink/F6RCK" alt="" />
+          </span>
+        </div>
+      </div>
+      <div className="col-8 h-100 mt-5">
+        <div className="row justify-content-center mb-1">
+          <div className="col-2 contantInfo">
+            <div>מבנה מל"י</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>גרף מל"י</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>בטיחות באימונים</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>עזרי אימון</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>תראו"גים</div>
+          </div>
+        </div>
+        <div className="row justify-content-center ">
+          <div className="col-2 contantInfo">
+            <div>שטחי אימונים</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>"שביל הזהב"</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>רב מערכתי</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>בקרת כניסה</div>
+          </div>
+          <div className="col-2 contantInfo">
+            <div>תפיסות וספרות</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ project/src/comps/topNav/TopNav.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./topNav.css";
+import Zroha from "../../imagesMaly/zrohaHayabasha.png";
+import Mishan from "../../imagesMaly/mishan.png";
+
+export default function TopNav() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-4 d-flex mt-3">
+          <img className="logos" src={Zroha} alt="" />
+          <img className="logos" src={Mishan} alt="" />
+          <span>
+            <h1 className="mt-2">מל"י דיגיטלי</h1>
+            <h3 className="d-flex align-items-center justify-content-center">
+              יולי 2023
+            </h3>
+          </span>
+        </div>
+        <div className="col-8 leftNav d-flex justify-content-center align-items-center">
+            <img className="logos" src={Zroha} alt="" />
+            <img className="logos" src={Zroha} alt="" />
+            <img className="logos" src={Zroha} alt="" />
+            <img className="logos" src={Zroha} alt="" />
+            <img className="logos" src={Zroha} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
