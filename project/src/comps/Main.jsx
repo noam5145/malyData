22d7bce8229acba9a5cda182adbfc8a46432144a
noam5145 <109206563+noam5145@users.noreply.github.com>
2023-08-01 10:27:51 +0300

    v

--- project/src/comps/Main.jsx
+++ project/src/comps/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TopNav from './topNav/TopNav';
 import InformationMaly from './informationMaly/InformationMaly';
 
+
 export default function Main() {
   return (
     <div className=''>
