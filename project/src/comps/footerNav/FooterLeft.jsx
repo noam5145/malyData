1345ff2773645cc92cd75a5992e1e3a66a1e8a38
Nehoray <nehorayatia1@gmail.com>
2023-08-01 14:40:31 +0300

    דף כניסה סיום

--- project/src/comps/footerNav/FooterLeft.jsx
+++ project/src/comps/footerNav/FooterLeft.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import "./Footer.css"
 import { Link } from 'react-router-dom'
+import  darom from "../../imagesMaly/pikudDarom.png"
+import  pikudMercaz from "../../imagesMaly/pikudMercaz.png"
+import  pikudZafon from "../../imagesMaly/pikudZafon.png"
+
 export default function FooterLeft() {
     return (
         <>
@@ -10,7 +14,7 @@ export default function FooterLeft() {
                         ענן מתאמנים מל"י
                     </p>
                 </div>
-                <div className="col-3 d-flex justify-content-center">
+                <div className="col-3  d-flex justify-content-center">
                     <Link className="">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg" alt=",sdc" />
                         <p className='mt-1'>זרוע יבשה</p>
@@ -18,20 +22,20 @@ export default function FooterLeft() {
                 </div>
                 <div className="col-3 d-flex justify-content-center">
                     <Link className="">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg" alt=",sdc" />
-                        <p className='mt-1'>זרוע יבשה</p>
+                        <img src={darom} alt=",sdc" />
+                        <p className='mt-1'>פיקוד דרום</p>
                     </Link>
                 </div>
                 <div className="col-3 d-flex justify-content-center">
                     <Link className="">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg" alt=",sdc" />
-                        <p className='mt-1'>זרוע יבשה</p>
+                        <img src={pikudMercaz} alt=",sdc" />
+                        <p className='mt-1'>פיקוד מרכז</p>
                     </Link>
                 </div>
                 <div className="col-3 d-flex justify-content-center">
                     <Link className="">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5a/IDF_GOC_Army_Headquarters_From_2020_%28Alternative%29.svg" alt=",sdc" />
-                        <p className='mt-1'>זרוע יבשה</p>
+                      <img src={pikudZafon} alt="" />
+                        <p className='mt-1'>פיקוד צפון</p>
                     </Link>
                 </div>
             </div>
--- project/src/comps/topNav/TopNav.jsx
+++ project/src/comps/topNav/TopNav.jsx
@@ -4,6 +4,7 @@ import Zroha from "../../imagesMaly/zrohaHayabasha.png";
 import Hativa500 from '../../imagesMaly/hativa500.png'
 import Mishan from "../../imagesMaly/mishan.png";
 
+
 export default function TopNav() {
   return (
     <div>
@@ -11,12 +12,12 @@ export default function TopNav() {
         <div className="col-4 d-flex mt-3">
           <img className="logos" src={Zroha} alt="" />
           <img className="logos" src={Hativa500} alt="" />
-          <span>
+          <div>
             <h1 className="mt-2">מל"י דיגיטלי</h1>
             <h3 className="d-flex align-items-center justify-content-center">
               יולי 2023
             </h3>
-          </span>
+          </div>
         </div>
         <div className="col-8 leftNav d-flex justify-content-center align-items-center">
             <img className="logos" src={Zroha} alt="" />
